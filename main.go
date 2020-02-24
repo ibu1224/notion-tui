865d6bdbe8bbb129a3f6d14d8e5aafea342ded8b
Ibuki Miyagami <ibuki.tdu1224@gmail.com>
2020-02-25 08:15:16 +0900

    prototype

--- main.go
+++ main.go
@@ -1,8 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gdamore/tcell"
+	"github.com/rivo/tview"
+)
 
 func main() {
+	app := tview.NewApplication()
+	root := tview.NewTreeNode(".").SetColor(tcell.ColorLawnGreen)
+	tree := tview.NewTreeView().SetRoot(root).SetCurrentNode(root)
 
-	fmt.Println(treeView)
+	add := func(target *tview.TreeNode, path string) {
+		// files, err := ioutil.ReadDir(path)
+		// if err != nil {
+		// panic(err)
+		// }
+		test := [3]string{"abc", "hoge", "pup"}
+		for _, file := range test {
+			node := tview.NewTreeNode(file).SetColor(tcell.Color100).
+				SetReference(file)
+			// SetSelectable(file)
+			// if file.IsDir() {
+			// 	node.SetColor(tcell.ColorRebeccaPurple)
+			// }
+			target.AddChild(node)
+		}
+	}
+
+	add(root, ".")
+	frame := tview.NewFlex().
+		AddItem(tree, 0, 1, true).
+		// AddItem(tview.NewFlex.SetBorder(true).SetBorderColor(tcell.ColorLawnGreen).SetTitle("treeView").SetTitleColor(tcell.Color100), 0, 1, false).
+		AddItem(tview.NewFlex().SetDirection(tview.FlexRow).
+			AddItem(tview.NewBox().SetBorder(true).SetTitle("Main content"), 0, 3, false), 0, 3, false)
+
+	if err := app.SetRoot(frame, true).Run(); err != nil {
+		panic(err)
+	}
 }
--- treeview.go
+++ treeview.go
@@ -1,48 +1,63 @@
+// Demo code for the TreeView primitive.
 package main
 
 import (
+	"io/ioutil"
+	"path/filepath"
+
 	"github.com/gdamore/tcell"
 	"github.com/rivo/tview"
 )
 
-func treeView() tview {
-
-	// newPrimitive := func(text string) tview.Primitive {
-	// 	return tview.NewTextView().
-	// 		SetTextAlign(tview.AlignCenter).
-	// 		SetText(text)
-	// }
-	menu := newPrimitive("Menu")
-	// main := newPrimitive("Main content")
-
-	// grid := tview.NewGrid().
-	// 	SetRows(3, 0, 3).
-	// 	SetColumns(30, 0, 30).
-	// 	SetBorders(true).
-	// 	AddItem(newPrimitive("Header"), 0, 0, 1, 3, 0, 0, false).
-	// 	AddItem(newPrimitive("Footer"), 2, 0, 1, 3, 0, 0, false)
-
-	// Layout for screens narrower than 100 cells (menu and side bar are hidden).
-
+// Show a navigable tree view of the current directory.
+func treeView() string {
 	rootDir := "."
 	root := tview.NewTreeNode(rootDir).
-		SetColor(tcell.ColorSlateGray)
-	tree := tview.NewTreeView().SetRoot(root).SetCurrentNode(root)
+		SetColor(tcell.ColorRed)
+	tree := tview.NewTreeView().
+		SetRoot(root).
+		SetCurrentNode(root)
 
+	// A helper function which adds the files and directories of the given path
+	// to the given target node.
 	add := func(target *tview.TreeNode, path string) {
-		mydata := [3]string{"hoge", "fuga", "piyo"}
-		for _, file := range mydata {
-			node := tview.NewTreeNode(file)
-			node.SetColor(tcell.ColorMediumAquamarine)
+		files, err := ioutil.ReadDir(path)
+		if err != nil {
+			panic(err)
+		}
+		for _, file := range files {
+			node := tview.NewTreeNode(file.Name()).
+				SetReference(filepath.Join(path, file.Name())).
+				SetSelectable(file.IsDir())
+			if file.IsDir() {
+				node.SetColor(tcell.ColorGreen)
+			}
 			target.AddChild(node)
 		}
 	}
-	// }
+
 	// Add the current directory to the root node.
 	add(root, rootDir)
-	return tree
 
-	// if err := tview.NewApplication().SetRoot(tree, true).Run(); err != nil {
-	// 	panic(err)
-	// }
+	// If a directory was selected, open it.
+	// tree.SetSelectedFunc(func(node *tview.TreeNode) {
+	// 	reference := node.GetReference()
+	// 	if reference == nil {
+	// 		return // Selecting the root node does nothing.
+	// 	}
+	// 	children := node.GetChildren()
+	// 	if len(children) == 0 {
+	// 		// Load and show files in this directory.
+	// 		path := reference.(string)
+	// 		add(node, path)
+	// 	} else {
+	// 		// Collapse if visible, expand if collapsed.
+	// 		node.SetExpanded(!node.IsExpanded())
+	// 	}
+	// })
+
+	if err := tview.NewApplication().SetRoot(tree, true).Run(); err != nil {
+		panic(err)
+	}
+	return "treeView"
 }
